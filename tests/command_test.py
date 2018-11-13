7723669ca227efe4841746bce59284c60a764334
Nick Hwangbo <Nick.Hwangbo@wdc.com>
2018-11-13 14:43:57 -0800

    Skeleton of pytest

--- tests/command_test.py
+++ tests/command_test.py
@@ -1,2 +1,16 @@
-import src.commands.py
+import sys, os
+sys.path.insert(0, os.path.abspath('../src'))
 
+import commands
+
+class TestMessage(object):
+	def test_one(self):
+		assert 1 == 1
+
+class TestHandleCommand(object):
+	def test_one(self):
+		assert 2 == 2
+
+class TestHandleMetaCommand(object):
+	def test_one(self):
+		assert 3 == 3
