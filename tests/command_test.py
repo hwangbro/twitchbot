add59f565625bd81b308563eb6c703a5073a0ab2
Nick Hwangbo <Nick.Hwangbo@wdc.com>
2018-11-13 16:11:18 -0800

    Started initial unit testing.

--- tests/command_test.py
+++ tests/command_test.py
@@ -14,3 +14,14 @@ class TestHandleCommand(object):
 class TestHandleMetaCommand(object):
 	def test_one(self):
 		assert 3 == 3
+
+class TestSpaceCommand(object):
+	def test_one(self):
+		test_input = 'haHAA ABC'
+		output = commands.space_command(test_input)
+		assert output == 'A haHAA B haHAA C'
+
+	def test_two(self):
+		test_input = 'LUL Hi Mom'
+		output = commands.space_command(test_input)
+		assert output == 'H LUL i LUL   LUL M LUL o LUL m'
