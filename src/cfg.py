3a35c7bc10ed2c6065af76d54ee545472ca3dcbf
nhwangbo <nickhwangbo@gmail.com>
2018-11-25 16:32:20 -0800

    Cleaned up cfg

--- src/cfg.py
+++ src/cfg.py
@@ -8,7 +8,6 @@ PORT = 6667
 NICK = 'xd_bot_xd'
 PASS = 'oauth:qfo55414g1bnak5x0x40merhmm42yb'  # used for irc
 CHAN = '#hwangbroxd'
-RATE = (2/3)  # messages per sec
 ADMIN = ['hwangbroxd', 'unlord1', 'gay_zach']
 CLIENT_ID = 'zfr9wyvljqfa3wn7acgse9bvo3xz2b'
 secret = 'rvs7zybcbiksg969eyt34mj27nzpld'
@@ -19,4 +18,3 @@ HEADERS = {'Client-ID': CLIENT_ID,
            'Accept': 'application/vnd.twitchtv.v5+json',
            'Authorization': f'OAuth {TOKEN}'}
 SCOPES = 'channel_editor+channel_read'
-CHAT_MSG = re.compile(r'^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :')
